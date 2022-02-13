371eef5d07ae1eee6568735ed8146bb4fe3009fd
Anton Kozyriev <sonara4mus@gmail.com>
2022-02-13 18:00:13 +0200

    feat(sync_server): record new processing request

--- src/sync_server/server.py
+++ src/sync_server/server.py
@@ -1,19 +1,71 @@
-from fastapi import FastAPI, APIRouter, Depends
+import uuid
+
+from fastapi import FastAPI, Form, File, Header, UploadFile, HTTPException, status
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from bson.binary import Binary
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import ASCENDING
+from datetime import datetime
 
-from validators import get_qr_token_header
+from settings import MAX_FILE_SIZE, MONGO_CLIENT_URL
 
 
-api_router = APIRouter(
-    prefix="/api",
-    tags=["api"],
-    dependencies=[Depends(get_qr_token_header)],
-    responses={404: {"description": "Not found"}},
-)
-
+accepted_content_types = ["image/jpeg", "image/png"]
 app = FastAPI(docs_url=None, redoc_url=None)
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+connection = AsyncIOMotorClient(MONGO_CLIENT_URL)
+app_database = connection.neuroparty
+requests_collection = app_database.requests
+
+
+@app.on_event("startup")
+def startup_event():
+    requests_collection.create_index([('token', ASCENDING)], unique=True)
+
+
+@app.on_event("shutdown")
+def shutdown_event():
+    connection.close()
+
+
+@app.post("/api/request")
+async def request(content_length: int = Header(None),
+                  file: UploadFile = File(...),
+                  token: str = Form(...),
+                  prompt: str = Form(...)):
+    if content_length is None:
+        raise HTTPException(status_code=status.HTTP_411_LENGTH_REQUIRED)
+
+    if content_length > MAX_FILE_SIZE:
+        raise HTTPException(status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE)
+
+    if file.content_type not in accepted_content_types:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid content type. Acceptable types are: {', '.join(accepted_content_types)}.")
+
+    try:
+        request_token = uuid.UUID(token)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Token '{token}' format is invalid. Expected to be UUID4.")
+
+    encoded_file = Binary(await file.read())
+
+    await file.close()
+
+    request_entity = {
+        "token": str(request_token),
+        "prompt": prompt,
+        "utc_request": datetime.utcnow(),
+        "image_name": file.filename,
+        "image_filetype": file.content_type,
+        "image_binary": encoded_file
+    }
+
+    db_result = await requests_collection.insert_one(request_entity)
+    return {
+        "post_id": str(db_result.inserted_id)
+    }
 
 
 @app.get("/")
---
+++ src/sync_server/settings.py
@@ -0,0 +1,5 @@
+import os
+
+
+MAX_FILE_SIZE = int(os.getenv("ENV_MAX_FILE_SIZE"))
+MONGO_CLIENT_URL = os.getenv("ENV_MONGO_CLIENT_URL")
